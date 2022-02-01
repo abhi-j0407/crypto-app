082b001b95cb2168d70a712ba9d109d43765ea97
Abhishek Jain <abhishek.j0407@gmail.com>
2022-02-01 15:43:01 +0530

    Card_Data imported successfully

--- src/components/CryptoCard.js
+++ src/components/CryptoCard.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import image from '../images/currency-exchange.png'
 
-const CryptoCard = () => {
+const CryptoCard = (props) => {
+
+  // console.log("Card data:",props)
+
   return ( 
   <div className='crypto-card'>
     <div className='crypto-card-header'>
-        <h3>1. Bitcoin</h3>
-        <img src={image} className='crypto-icon' alt="crypto-img" />
+        <h3>{props.data.rank}. {props.data.name}</h3>
+        <img src={props.data.iconUrl} className='crypto-icon' alt="crypto-img" />
     </div>
     <hr />
     <div className='crypto-content'>
-        <p className='card-stat'>PRice : 23121</p>
-        <p className='card-stat'>PRice : 23121</p>
-        <p className='card-stat'>PRice : 23121</p>
+        <p className='card-stat'>Price : {props.data.price}</p>
+        <p className='card-stat'>Market Cap : {props.data.marketCap}</p>
+        <p className='card-stat'>Daily Change : {props.data.change}%</p>
     </div>
   </div>
   )
--- src/components/CryptoSummary.js
+++ src/components/CryptoSummary.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 
 import CryptoCard from './CryptoCard';
 
+var axios = require("axios").default;
+
 const CryptoSummary = () => {
-  
     
-        const cData = [
-            { id : 1},
-            { id : 2},
-        ];
+    const [coins,setCoins] = useState([]);
+
+    var options = {
+        method: 'GET',
+        url: 'https://coinranking1.p.rapidapi.com/coins',
+        params: {
+          referenceCurrencyUuid: 'yhjMzLPhuIDl',
+          timePeriod: '24h',
+          tiers: '1',
+          orderBy: 'marketCap',
+          orderDirection: 'desc',
+          limit: '50',
+          offset: '0'
+        },
+        headers: {
+          'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+          'x-rapidapi-key': 'f53cf2dba5msh627f70cee70049dp1f6753jsn02e04ed65bf0'
+        }
+      };
+
+      useEffect(() => {
+        axios.request(options).then((response) => {
+            setCoins(response.data.data.coins);
+        }).catch((error) => {
+            console.error(error);
+        });
+      },[])
+
+      
+
+      // useEffect(() => {
+      //   console.log("Data coin:",coins);
+      // },[coins])
 
     return (
   <div className='cryptosummary'>
@@ -17,11 +47,11 @@ const CryptoSummary = () => {
         <h2 className='show-more'>Show more</h2>
     </div>
     <div className='crypto-container'>
-        {
-            cData.map((n) => (
-                <CryptoCard key = {n.id} />
+         {
+            coins.map((n) => (
+                <CryptoCard key = {n.rank} data = {n} />
             ))
-        }
+        } 
     </div>
   </div>
   )
